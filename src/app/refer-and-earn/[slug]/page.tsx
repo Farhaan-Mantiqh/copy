f7b224160bd7580a315c7fb94c0a0dbef86a0f1f
farhaan <farhaan.mantiqh@gmail.com>
2026-07-28 19:55:03 +0530

    feat: added redirect  static params

--- src/app/refer-and-earn/[slug]/page.tsx
+++ src/app/refer-and-earn/[slug]/page.tsx
@@ -1,12 +1,5 @@
 import RedirectClient from './RedirectClient';
 
-export function generateStaticParams(): { slug: string }[] {
-  return [
-    { slug: "referal-code" }
-  ]
-}
-
-
 export default function AppRedirectPage(): JSX.Element {
   return <RedirectClient />;
 }
