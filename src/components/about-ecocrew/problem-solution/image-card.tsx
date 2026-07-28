1b94fa5fe062c612977226d40dbf623df21258ef
farhaan <farhaan.mantiqh@gmail.com>
2026-07-28 15:22:07 +0530

    feat: img tag insted of image tag

--- src/components/about-ecocrew/problem-solution/image-card.tsx
+++ src/components/about-ecocrew/problem-solution/image-card.tsx
@@ -1,5 +1,5 @@
+'use client'
 import type { StaticImageData } from 'next/image';
-('use client');
 
 type ImageCardProps = {
   image: StaticImageData | string;
--- src/components/single-blog/index.tsx
+++ src/components/single-blog/index.tsx
@@ -1,5 +1,5 @@
+'use client';
 import type { StaticImageData } from 'next/image';
-('use client');
 
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
